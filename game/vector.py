55def6424ddb7f8cf888acad043a65c55eb4cb61
Christian Prior <chris.prior.uk@gmail.com>
2019-05-30 10:39:42 +0100

    add vector file

---
+++ game/vector.py
@@ -0,0 +1,19 @@
+class Vec2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __add__(self, other):
+        return Vec2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vec2(self.x - other.x, self.y - other.y)
+
+    def __truediv__(self, other):
+        return Vec2(self.x / other, self.y / other)
+
+    def __floordiv__(self, other):
+        return Vec2(self.x // other, self.y // other)
--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import pyxel
 
+
 class App:
     def __init__(self):
-        pyxel.init(160, 120)
+        pyxel.init(255, 255)
+        pyxel.load("assets/sprites.pyxel")
+
         self.x = 0
         pyxel.run(self.update, self.draw)
 
