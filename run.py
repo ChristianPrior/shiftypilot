5c97eaba5a73d3167fe6b22d9ad5f9a1b933b01a
Christian Prior <chris.prior.uk@gmail.com>
2019-05-31 18:47:53 +0100

    fix game name

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ from game.projectile import Meteor
 from game.vector import Vec2
 
 ALPHABET = string.ascii_uppercase
-GAME_NAME = "Shitty Pilot 1: Galactic Apocalypse"
+GAME_NAME = "Shifty Pilot 1: Galactic Apocalypse"
 SIZE = Vec2(320, 320)
 
 ASSETS_PATH = f"{os.getcwd()}/assets/sprites.pyxel"
